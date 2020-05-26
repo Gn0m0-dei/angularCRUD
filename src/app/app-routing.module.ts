402af5e8530612c62cc0793bece2bb370a454b3f
DavidC <dcv.dcv@protonmail.com>
2020-05-26 10:21:06 +0200

    UPDATE: configurado el routing de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { EditCardComponent } from './components/edit-card/edit-card.component';
+import { NewCardComponent } from './components/new-card/new-card.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListCardsComponent },
+  { path: 'edit/:id', component: EditCardComponent },
+  { path: 'new', component: NewCardComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
