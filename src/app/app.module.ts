3b82c787237cee9262977e1a81027c52189f6db4
DavidC <dcv.dcv@protonmail.com>
2020-05-26 10:20:32 +0200

    UPDATE: limpiado el appComponent y e insertado un link para los iconos de google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ConnectionService } from './services/connection.service';
+import { CardComponent } from './components/card/card.component';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { NewCardComponent } from './components/new-card/new-card.component';
+import { EditCardComponent } from './components/edit-card/edit-card.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ListCardsComponent,
+    NewCardComponent,
+    EditCardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
