9ad4fe5179789b67d6a67f86f5a251d13c657891
DavidC <dcv.dcv@protonmail.com>
2020-05-26 10:22:11 +0200

    NEW: creado el servicio y confugurado el get con manejador de errores

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { People } from '../classes/people';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionService {
 
-  constructor() { }
+  readonly urlApi: string = 'http://hello-world.innocv.com/api/user';
+
+  constructor(private http: HttpClient) { }
+
+  getPeople(): Observable<People[]> {
+    return this.http.get<People[]>(this.urlApi).pipe(catchError(this.errorHandler))
+  }
+
+  errorHandler(err: HttpErrorResponse) {
+    return throwError(err.message || 'Server error');
+  }
 }
