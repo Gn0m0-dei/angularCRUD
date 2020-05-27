3669a37bed2e53e2e777203410f8489fa1269642
DavidC <dcv.dcv@protonmail.com>
2020-05-27 10:22:18 +0200

    UPDATE: configurado el componente new card

--- src/app/components/new-card/new-card.component.ts
+++ src/app/components/new-card/new-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { People } from '../../classes/people';
+import { ConnectionService } from '../../services/connection.service';
+
+declare var M: any;
 
 @Component({
   selector: 'app-new-card',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCardComponent implements OnInit {
 
-  constructor() { }
+  content: People;
+
+  constructor(private api: ConnectionService) { }
 
   ngOnInit(): void {
+    this.content = new People();
+  }
+
+  createPerson() {
+    this.api.postPerson(this.content).subscribe(res => {
+      this.content = new People();
+      M.toast({html: 'New user created', classes: 'rounded green'});
+    }), (err => {
+      M.toast({html: err, classes: 'rounded red'});
+    })
   }
 
 }
