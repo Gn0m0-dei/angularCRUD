2d901a9ef14679bef1addea33265748b7d985838
DavidC <dcv.dcv@protonmail.com>
2020-05-27 10:22:34 +0200

    UPDATE: configurado el componente edit card

--- src/app/components/edit-card/edit-card.component.ts
+++ src/app/components/edit-card/edit-card.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { People } from '../../classes/people';
+import { ActivatedRoute } from '@angular/router';
+import { ConnectionService } from '../../services/connection.service';
+
+declare var M: any;
 
 @Component({
   selector: 'app-edit-card',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCardComponent implements OnInit {
 
-  constructor() { }
+  content: People;
+  loaded: boolean = false;
+
+
+  constructor(private api: ConnectionService,
+              private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.getPerson(+id);
+  }
+
+  getPerson(id: number) {
+    this.api.getPerson(id).subscribe(res => {
+      this.content = res as People;
+      this.loaded = true;
+    }, (err) => {
+      M.toast({html: err, classes: 'rounded red'});
+    })
+  }
+
+  updatePerson() {
+    this.loaded = false;
+    this.api.putPerson(this.content).subscribe(res => {
+      this.content = res as People || this.content;
+      this.loaded = true;
+      M.toast({html: 'Update Done', classes: 'rounded green'});
+    }, (err) => {
+      M.toast({html: err, classes: 'rounded red'});
+    })
+  }
+
+  deletePerson(id: number) {
+    this.api.deletePerson(id).subscribe( res => {
+      this.api.update.emit();
+    }, (err) => {
+      M.toast({html: err, classes: 'rounded red'});
+    })
   }
 
 }
